e3b660cb6699fd6aa708c15f4425256770e52eb7
Luke <gianluca.suriani@outlook.it>
2020-10-11 12:00:51 +0200

    Fixed yet another bug!

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -16,13 +16,13 @@ module.exports = {
         return message
           .reply(cross + " That doesn't seem to be a valid number.")
           .then(msg => {
-            msg.delete(5000);
+            msg.delete({timeout: 5000});
           });
       } else if (amount < 2 || amount > 100) {
         return message
           .reply(cross + "you need to input a number between 2 and 100.")
           .then(msg => {
-            msg.delete(5000);
+            msg.delete({timeout: 5000});
           });
         message.delete();
       }
@@ -36,14 +36,14 @@ module.exports = {
               "There was an error, perhaps you forgot to give me the **Manage Messages** permission?"
           )
           .then(msg => {
-            msg.delete(5000);
+            msg.delete({timeout: 5000});
           });
       });
 
       message.channel
         .send(tick + " Deleted " + amount + " message/s!")
         .then(msg => {
-          msg.delete(5000);
+          msg.delete({timeout: 5000});
         });
     } else {
       message.channel.send(
