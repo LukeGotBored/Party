259f42fd7931984c1a396478ab9170aa1e2a4762
Luke <gianluca.suriani@outlook.it>
2020-10-11 11:41:00 +0200

    Updated Ban Command

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -9,51 +9,81 @@ module.exports = {
   async execute(message, args) {
     const tick = message.client.emojis.cache.get("655807079784644608").toString()
     const cross = message.client.emojis.cache.get("655807081240330245").toString()
-
-    var user = message.mentions.users.first(); // returns the user object if an user mention exists
-    var banReason = args.slice(1).join(" "); // Reason of the ban (Everything behind the mention)
-    if (user === message.author) return message.channel.send(cross + ' Hold on did you just try to ban yourself?');
-    if (!message.member.hasPermission("BAN_MEMBERS")){
-      return message.channel.send(cross + "You don't have the **Ban Members** permission!")
-    }
     
-    if (message.guild.me.hasPermission('BAN_MEMBERS', false, false)){
-      return message.channel.send(cross + "I don't have the **Ban Members** permission!")
-    }
+    let banned = message.mentions.users.first() || message.client.users.resolve(args[0]);
+    let reason = args.slice(1).join(" ") + " | banned by " + message.author.username;
     
-    if (!user) {
-      try {
-        if (!message.client.users.get(args.slice(0, 1).join(" ")))
-          throw new Error(cross + " I couldn't find that user!");
-          user = message.guild.members.get(args.slice(0, 1).join(" "));
-          user = user.user;
-        
-      } catch (error) {
-        return message.channel.send(cross + " **" + message.author.username + "**, you didn't provide a valid user to ban!");
-      }
-    }
     
-   
-    if (!message.guild.member(user).bannable) return message.reply(cross + ' I can\'t ban a moderator!');
-
-    if (!banReason) {
-      banReason = "Unspecified";
-    }
-    
-   const banEmb = new Discord.MessageEmbed()
-        .setColor("0xfeb637")
-        .setAuthor("Banned by " + message.author.username , message.author.displayAvatarURL({type: "png", size: 1024}))
-        .setTitle(user.user.tag + " has been kicked!")
-        .setThumbnail(user.user.displayAvatarURL({type: "png", size: 1024}))
-        .addField("Reason:", banReason, true)
-        .setTimestamp()
-        .setFooter("Party!", "https://i.imgur.com/B6QKBgC.png");
-
-    
+        if (message.author === banned) {
+      let sanctionyourselfembed = new Discord.MessageEmbed()
+        .setDescription(`${cross} You cannot ban yourself!`)
+        .setColor("#ff0000");
+      message.channel.send(sanctionyourselfembed);
   
-    await message.guild.ban(user, { reason: banReason })
-      .then(
-        message.channel.send(banEmb))
-      .catch(console.error);
+      return;
+    }
+  
+    if (!reason) {
+      let noreasonembed = new Discord.MessageEmbed()
+        reason = "not specified | banned by " + message.author.username
+    }
+  
+    if (!message.member.permissions.has("BAN_MEMBERS")) {
+      let nopermsembed = new Discord.MessageEmbed()
+        .setDescription(
+          cross + " You do not have permission `BAN MEMBERS` contact an administrator"
+        )
+        .setColor("ff0000");
+      message.channel.send(nopermsembed);
+  
+      return;
+    }
+  
+    if (!message.guild.me.permissions.has("BAN_MEMBERS")) {
+      let botnopermsembed = new Discord.MessageEmbed()
+        .setDescription(
+          cross + " I do not have `BAN MEMBERS` permission"
+        )
+        .setColor("#ff0000");
+      message.channel.send(botnopermsembed);
+  
+      return;
+    }
+  
+    
+    if(!message.guild.member(banned.id).bannable){
+      let usermodembed = new Discord.MessageEmbed()
+        .setDescription(
+          cross + " I can't ban a moderator!"
+        )
+        .setColor("#ff0000");
+      message.channel.send(usermodembed);
+  
+      return;
+    }
+
+    
+    
+    message.guild.members.ban(banned, { reason: reason });
+  
+    let successfullyembed = new Discord.MessageEmbed()
+       .setColor("0xfeb637")
+       .setAuthor("Banned by " + message.author.username , message.author.displayAvatarURL({type: "png", size: 1024}))
+       .setTitle(`${tick} ${banned.tag} has been banned!`)
+       .setThumbnail(banned.displayAvatarURL({type: "png", size: 1024}))
+       .addField("Reason:", reason, true)
+       .setTimestamp()
+       .setFooter("Party!", "https://i.imgur.com/B6QKBgC.png");
+  
+    message.channel.send(successfullyembed);
+ 
+    
+    
+    
+    
+    
+    
+  const banEmb = new Discord.MessageEmbed()
+       
   }
 };
